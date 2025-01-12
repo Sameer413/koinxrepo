43aa4b3e857fe108158e230c30b875cda46a6a29
Sameer413 <92072016+Sameer413@users.noreply.github.com>
2025-01-12 16:51:39 +0530

    Initial

--- app/components/CryptoInfo.tsx
+++ app/components/CryptoInfo.tsx
@@ -4,7 +4,7 @@ import LineChart from "./LineChart";
 const CryptoInfo = () => {
   return (
     <div className="bg-white rounded-md p-10 mb-5">
-      <div className="">Didn't got more time because, there a function in my home.</div>
+      <div className="">{"Didn't got more time because, there a function in my home."}</div>
       <div className="text-2xl text-[#0F1629] font-semibold">Tokenomics</div>
 
       <div className="">
